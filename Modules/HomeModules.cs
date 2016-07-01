3b2b39110bf65d6386a2e363edfdeb2c7f621ec3
Matthew Reyes <mreyez@gmail.com>
2016-07-01 09:06:18 -0700

    add created_contact, add_contact and view_contacts.cshtml and content

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -12,6 +12,14 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/all_contacts"] = _ => {
+          List<Contact> allContacts = Contact.GetContacts();
+          return View["view_contacts.cshtml", allContacts];
+      };
+      Post["/add_contact"] = _ => {
+          Contact newContact = new Contact(Request.Form[""], Request.Form[""], Request.Form[""]);
+          return View["created_contact.cshtml", newContact];
+      };
     }
   }
 }
